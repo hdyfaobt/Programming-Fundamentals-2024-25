31a5b229774cf0da291a4aeae42d36d746841b26
hdyfaobt <houdeyfaoulad@gmail.com>
2024-10-03 15:29:01 +0200

    lussen

---
+++ Programming Fundamentals/Module3-lussen/tstJezelf1.mjs
@@ -0,0 +1,10 @@
+let getal;
+let som=0;
+
+for(som = 0; som <= 100; som+=getal){
+
+    getal = parseFloat(await userInput.question('geef een getal in: '));
+    
+
+}
+
---
+++ Programming Fundamentals/Module3-lussen/tstJzlf5.mjs
@@ -0,0 +1,11 @@
+let i = 1;
+let j = 0;
+while (i<=3) {
+    console.log(i)
+    i++
+    
+}
+while (j<=6) {
+        console.log(j)
+        j++
+    }
